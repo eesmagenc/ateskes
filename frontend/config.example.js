ae55ba223327a46a070ecca08ad22ff8e7a82d28
Sudenur Cakir <sudenurcakir1@gmail.com>
2026-09-08 19:01:26 +0300

    Hafta 1-2: Leaflet harita, GeoJSON, GIS style engine, filtre paneli, detay popup

---
+++ frontend/config.example.js
@@ -0,0 +1,7 @@
+// Gerçek key için Sude'ye sor (CARTO ücretsiz demo key, ekip içi paylaşılıyor).
+// Kendi key'ini almak istersen: https://carto.com/ (ücretsiz kayıt)
+const CARTO_KEY = "BURAYA_KENDI_CARTO_KEYINI_YAZ";
+
+// Yerelde geliştirirken localhost, canlıya (Render/Railway) deploy
+// edilince Esma'nın verdiği gerçek URL ile değiştirilecek.
+const BACKEND_URL = "http://localhost:8000/bolgeler";
